69cc1dba484795c24d8c287f1e76559bb140a313
Rodrigo Castro <rodrigo.castro@avira.com>
2021-10-29 22:04:15 +0200

    Adjust markdown links and text selectable

--- lib/posts/widgets/posts_populated.dart
+++ lib/posts/widgets/posts_populated.dart
@@ -2,6 +2,7 @@ import 'package:blog/posts/posts.dart';
 import 'package:blog/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class PostsPopulated extends StatelessWidget {
   const PostsPopulated({Key? key, required this.posts}) : super(key: key);
@@ -21,6 +22,9 @@ class PostsPopulated extends StatelessWidget {
             ),
             subtitle: MarkdownBody(
               data: post.body,
+              selectable: true,
+              onTapLink: (text, href, title) =>
+                  href != null ? _launchURL(href) : null,
             ),
           ),
         );
@@ -29,3 +33,6 @@ class PostsPopulated extends StatelessWidget {
     );
   }
 }
+
+void _launchURL(String url) async =>
+    await canLaunch(url) ? await launch(url) : throw 'Could not launch $url';
